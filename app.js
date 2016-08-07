a859bd1004630734cc35f3c52c1c7976d37659a4
Boris Kliushyn <kljushin@gmail.com>
2016-08-07 13:39:14 +0300

    index

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.BOTFRAMEWORK_APPID,
+    appSecret: process.env.BOTFRAMEWORK_APPSECRET
 });
 
 var movieList = {
